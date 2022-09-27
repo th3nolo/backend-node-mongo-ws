69f3df3dc78ea680591c816f86c0f6bb1fd76621
Manuel Parra <th3nolo@gmail.com>
2022-09-27 16:56:29 -0400

    added nodemon locally, and body parser

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const { application } = require('express');
+const bodyParser = require('body-parser');
 const express = require('express');
 const router = express.Router();
 
 var app = express();
-
+app.use(bodyParser);
 app.use(router);
 
+
 router.get('/', (req,res) => res.send('Hola desde get'));
+
+router.post('/', (req,res) => res.send('Hola desde post'));
+
+router.delete('/', (req,res) => res.send('Hola desde delete'));
 
 //app.use('/', function(req, res){
 //    res.send('Hello World');
