6ce85b46a75ed800008298e211771d454a100a49
Manuel Parra <th3nolo@gmail.com>
2022-09-27 23:18:09 -0400

    added a static element to the API, and some new directories

--- server.js
+++ server.js
@@ -1,42 +1,43 @@
-const { application } = require('express');
-const express = require('express');
+const { application } = require("express");
+const express = require("express");
 const router = express.Router();
-const bodyParser = require('body-parser');
-const response = require('./network/response');
-
+const bodyParser = require("body-parser");
+const response = require("./network/response");
 
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 
-router.get('/message', (req, res) => {
-    console.log(req.headers);
-    console.log(req.body);
-    res.header({
-        "custom-header": "Nuestro valor personalizado"
-    })
-    //res.send('Lista de mensajes');
-    response.success(req, res, 'Lista de mensajes', req.statusCode);
-})
+router.get("/message", (req, res) => {
+  console.log(req.headers);
+  console.log(req.body);
+  res.header({
+    "custom-header": "Nuestro valor personalizado",
+  });
+  response.success(req, res, "Lista de mensajes", req.statusCode);
+});
 
-router.post('/message', (req, res) => {
-    console.log(req.query);
-    console.log(req.body);
-    if( req.query.error == "ok" ) {
-    response.error(req, res, 'Error simulado', 400);
-    }else{
-    response.success(req, res, 'Mensaje creado', 201);
-    }
-    //res.status(201).send({error: '', body: 'Mensaje creado'});    
-})
+router.post("/message", (req, res) => {
+  console.log(req.query);
+  console.log(req.body);
+  if (req.query.error) {
+    response.error(req, res, error, 400);
+  } else {
+    response.success(req, res, "Mensaje creado", 201);
+  }
+});
 
-router.delete('/message', (req, res) => {
-    console.log(req.query);
-    console.log(req.body);   
-    res.status(201).send({error: '', body: 'Mensaje eliminado'});
-})
+router.delete("/message", (req, res) => {
+  console.log(req.query);
+  console.log(req.body);
+  if (req.query.error) {
+    response.error(req, res, error, 400);
+  } else {
+    response.success(req, res, "Mensaje creado", 201);
+  }
+});
 
+app.use("/app", express.static("public"));
 app.listen(3000);
-console.log('Server running at http://localhost:3000/');
-
+console.log("Server running at http://localhost:3000/");
