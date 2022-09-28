6c6624764e91952a9d91b526affd396911d1ab49
Manuel Parra <th3nolo@gmail.com>
2022-09-27 20:00:13 -0400

    formas de enviar respuestas del servidor al cliente

--- server.js
+++ server.js
@@ -4,21 +4,31 @@ const express = require('express');
 const router = express.Router();
 
 var app = express();
-app.use(bodyParser);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 
+router.get('/message', (req, res) => {
+    console.log(req.headers);
+    console.log(req.body);
+    res.header({
+        "custom-header": "Nuestro valor personalizado"
+    })
+    res.send('Lista de mensajes');
+})
 
-router.get('/', (req,res) => res.send('Hola desde get'));
+router.post('/message', (req, res) => {
+    console.log(req.query);
+    console.log(req.body);
+    res.status(201).send({error: '', body: 'Mensaje creado'});
+})
 
-router.post('/', (req,res) => res.send('Hola desde post'));
-
-router.delete('/', (req,res) => res.send('Hola desde delete'));
-
-//app.use('/', function(req, res){
-//    res.send('Hello World');
-//})
-
-// app.use('/', (req,res) => res.send('Hello World')); // Another way to write the above code
+router.delete('/message', (req, res) => {
+    console.log(req.query);
+    console.log(req.body);
+    //res.send('Mensaje' + req.body.text + 'eliminado');
+    res.status(201).send({error: '', body: 'Mensaje eliminado'});
+})
 
 app.listen(3000);
 console.log('Server running at http://localhost:3000/');
